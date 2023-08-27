793bfca5fda27ad0fb2f91d4332335dfd0c52fb0
Shreyas4895 <jshreyas11@gmail.com>
2023-08-27 19:05:20 +0530

    code-refactor

--- src/Component/Adduser.js
+++ src/Component/Adduser.js
@@ -4,69 +4,101 @@ import { Link, useNavigate } from "react-router-dom";
 import { FunctionAddUser } from "../Redux/Action";
 
 const Adduser = () => {
-    const [name, namechange] = useState('');
-    const [email, emailchange] = useState('');
-    const [phone, phonechange] = useState('');
-    const [role, rolechange] = useState('staff');
-    const dispatch=useDispatch();
-    const navigate=useNavigate();
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    role: "staff",
+  });
 
-    
-    const handlesubmit = (e) => {
-        e.preventDefault();
-        const userobj = { name, email, phone, role };
-        dispatch(FunctionAddUser(userobj));
-        navigate('/user');
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserDetails((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-    return (
-        <div>
-            <form onSubmit={handlesubmit}>
-                <div className="card">
-                    <div className="card-header" style={{ textAlign: 'left' }}>
-                        <h2>Add User</h2>
-                    </div>
-                    <div className="card-body" style={{ textAlign: 'left' }}>
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="form-group">
-                                    <label>Name</label>
-                                    <input value={name} onChange={e => namechange(e.target.value)} className="form-control"></input>
-                                </div>
-                            </div>
-                            <div className="col-lg-12">
-                                <div className="form-group">
-                                    <label>Email</label>
-                                    <input value={email} onChange={e => emailchange(e.target.value)} className="form-control"></input>
-                                </div>
-                            </div>
-                            <div className="col-lg-12">
-                                <div className="form-group">
-                                    <label>Phone</label>
-                                    <input value={phone} onChange={e => phonechange(e.target.value)} className="form-control"></input>
-                                </div>
-                            </div>
-                            <div className="col-lg-12">
-                                <div className="form-group">
-                                    <label>Role</label>
-                                    <select value={role} onChange={e => rolechange(e.target.value)} className="form-control">
-                                        <option value="admin">Admin</option>
-                                        <option value="staff">Staff</option>
-                                    </select>
-                                </div>
-                            </div>
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-                        </div>
-                    </div>
-                    <div className="card-footer" style={{ textAlign: 'left' }}>
-                        <button className="btn btn-primary" type="submit">Submit</button> |
-                        <Link className="btn btn-danger" to={'/user'}>Back</Link>
-                    </div>
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    dispatch(FunctionAddUser(userDetails));
+    navigate("/user");
+  };
 
+  return (
+    <div>
+      <form onSubmit={handlesubmit}>
+        <div className="card">
+          <div className="card-header" style={{ textAlign: "left" }}>
+            <h2>Add User</h2>
+          </div>
+          <div className="card-body" style={{ textAlign: "left" }}>
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <label>Name</label>
+                  <input
+                    name="name"
+                    value={userDetails.name}
+                    onChange={handleChange}
+                    className="form-control"
+                  ></input>
                 </div>
-            </form>
+              </div>
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <label>Email</label>
+                  <input
+                    name="email"
+                    value={userDetails.email}
+                    onChange={handleChange}
+                    className="form-control"
+                  ></input>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <label>Phone</label>
+                  <input
+                    name="phone"
+                    value={userDetails.phone}
+                    onChange={handleChange}
+                    className="form-control"
+                  ></input>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <label>Role</label>
+                  <select
+                    name="role"
+                    value={userDetails.role}
+                    onChange={handleChange}
+                    className="form-control"
+                  >
+                    <option value="admin">Admin</option>
+                    <option value="staff">Staff</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card-footer" style={{ textAlign: "left" }}>
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>{" "}
+            |
+            <Link className="btn btn-danger" to={"/user"}>
+              Back
+            </Link>
+          </div>
         </div>
-    );
-}
+      </form>
+    </div>
+  );
+};
 
 export default Adduser;
